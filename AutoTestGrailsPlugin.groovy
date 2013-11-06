65a975f7fe65834b13f713fcbd32f96189b13dec
Burt Beckwith <burt@burtbeckwith.com>
2013-11-05 22:38:52 -0500

    moved the author names and emails to the developers list since the plugin publishing can fail if the email property isn't a single well-formed email

--- AutoTestGrailsPlugin.groovy
+++ AutoTestGrailsPlugin.groovy
@@ -1,14 +1,7 @@
 class AutoTestGrailsPlugin {
     def version = "1.0.1"
     def grailsVersion = "2.0.0 > *"
-    def dependsOn = [:]
 
-    def pluginExcludes = [
-		"grails-app/views/error.gsp"
-    ]
-
-    def author = "Bjoern Wilmsmann, Mike Hugo"
-    def authorEmail = "bjoern@metasieve.com, mike@piragua.com"
     def title = "Auto test plugin"
     def description = '''\
 Provides continuous testing for your Grails projects. Run a single command and then every time you save a file,\
@@ -17,6 +10,10 @@ the relevant tests will run. Great for instant feedback while you're coding.\
 
     def documentation = "http://grails.org/plugin/auto-test"
     def license = "APACHE"
+    def developers = [
+        [name: "Bjoern Wilmsmann", email: "bjoern@metasieve.com"],
+        [name: "Mike Hugo", email: "mike@piragua.com"]
+    ]
     def scm = [url: "https://github.com/mjhugo/grails-auto-test"]
     def issueManagement = [system: "JIRA", url: "http://jira.grails.org/browse/GPAUTOTEST"]
 }
